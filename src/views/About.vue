d4c7302824161ccaa0d50a9ba06452823357f084
eddie605134@gmail.com <eddie605134@gmail.com>
2022-01-22 22:52:30 +0800

    fix: remote

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ test }}
+    <card></card>
   </div>
 </template>
+
+<script>
+import card from '@/components/Card.vue'
+export default {
+  components: {
+    card
+  },
+  data () {
+    return {
+      test: 1
+    }
+  }
+
+}
+</script>
